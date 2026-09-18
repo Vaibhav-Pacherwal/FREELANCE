96be09cd0b9b8e9ede23042a90d9d9709647b6b8
Vaibhav Pacherwal <vaibhavpacherwal57@gmail.com>
2026-09-18 19:37:22 +0530

    last

--- gwears/backend/server.js
+++ gwears/backend/server.js
@@ -100,7 +100,7 @@ const PORT = process.env.PORT || 8080;
 const main = async () => {
     await connectToDB();
     
-    app.listen(PORT, () => {
+    app.listen(PORT, "0.0.0.0", () => {
         console.log(`Server is running on ${PORT}`);
     });
 };
